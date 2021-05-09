8aa16ba82dd13200f25ea36879f135c9d41a8966
cristiberzescu <berzescu.c31@gmail.com>
2021-05-09 17:32:14 +0200

    [ABZ-45] - Cycling button done.

--- src/main/java/org/reg/controllers/LoginController.java
+++ src/main/java/org/reg/controllers/LoginController.java
@@ -2,84 +2,75 @@ package org.reg.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import org.reg.exceptions.PasswordIncorrectException;
+import org.reg.exceptions.WrongPasswordException;
 import org.reg.services.UserService;
 
 import java.io.IOException;
+import java.util.Objects;
+
+import org.reg.model.User;
+
+import static org.reg.services.UserService.getUsers;
 
 public class LoginController {
 
     @FXML
-    private TextField usernameField;
+    TextField username;
     @FXML
-    private PasswordField passwordField;
+    TextField password;
     @FXML
-    private ChoiceBox<String> role;
+    private static String loggedUsername;
     @FXML
-    private Text message;
-    @FXML
-    private Button loginButton;
+    private static String loggedRole;
 
     @FXML
-    public void initialize() {
-        role.getItems().addAll("Customer", "Administrator");
-    }
-
-    @FXML
-    public void handleRegisterAction() {
+    public void handleLoginAction(javafx.event.ActionEvent homepage) throws IOException {
         try {
-            Stage stage = new Stage();
-
-            boolean test = UserService.checkUserDoesAlreadyExist(usernameField.getText(), passwordField.getText());
-            Stage primaryStage = (Stage) loginButton.getScene().getWindow();
-            primaryStage.close();
-            if(test) {
-                if(((String) role.getValue()).equals("Customer")) {
-                    FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("customerPage.fxml"));
-                    Parent root = (Parent) loader.load();
-                    stage.setScene(new Scene(root));
-                    stage.show();
-                } else if (((String) role.getValue()).equals("Administrator")) {
-                    FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("administratorPage.fxml"));
-                    Parent root = (Parent) loader.load();
-                    stage.setScene(new Scene(root));
-                    stage.show();
+            UserService.loginUser(username.getText(), password.getText());
+            for (User user : getUsers().find()) {
+                if(Objects.equals(username.getText(), user.getUsername())) {
+                    this.loggedUsername = user.getUsername();
+                    this.loggedRole = user.getRole();
                 }
             }
 
-            if(!test) {
-                if (((String) role.getValue()).equals("Customer")) {
-                    FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("registerCustomer.fxml"));
-                    Parent root = (Parent) loader.load();
-                    stage.setScene(new Scene(root));
-                    stage.show();
-                    RegisterControllerCustomer reg = loader.getController();
-                    reg.setUsernameField(usernameField);
-                    reg.setPasswordField(passwordField);
-                    reg.setRole(role);
-                } else if (((String) role.getValue()).equals("Administrator")) {
-                    FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("registerAdministrator.fxml"));
-                    Parent root = (Parent) loader.load();
-                    stage.setScene(new Scene(root));
-                    stage.show();
-                    RegisterControllerAdministrator reg = loader.getController();
-                    reg.setUsernameField(usernameField);
-                    reg.setPasswordField(passwordField);
-                    reg.setRole(role);
-                }
+            if(loggedRole.equals("Customer")) {
+                FXMLLoader Loader = new FXMLLoader();
+                Loader.setLocation(getClass().getClassLoader().getResource("customerPage.fxml"));
+                Parent viewCustomerPage = Loader.load();
+                Scene customerPageScene = new Scene(viewCustomerPage, 650, 450);
+                Stage window = (Stage) ((Node) homepage.getSource()).getScene().getWindow();
+                window.setScene(customerPageScene);
+                window.show();
             }
-        } catch (PasswordIncorrectException e) {
-            message.setText(e.getMessage());
-        } catch (IOException ee) {
-            System.out.println("Eroare!!");
+            if (loggedRole.equals("Administrator")) {
+                FXMLLoader Loader = new FXMLLoader();
+                Loader.setLocation(getClass().getClassLoader().getResource("administratorPage.fxml"));
+                Parent viewAdministratorPage = Loader.load();
+                Scene administratorPageScene = new Scene(viewAdministratorPage, 650, 450);
+                Stage window = (Stage) ((Node) homepage.getSource()).getScene().getWindow();
+                window.setScene(administratorPageScene);
+                window.show();
+            }
+        } catch (WrongPasswordException e) {
+            password.clear();
         }
     }
+
+    public void goToRegistration(javafx.event.ActionEvent register) throws IOException {
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getClassLoader().getResource("register.fxml"));
+        Parent viewRegister = Loader.load();
+        Scene RegisterScene = new Scene(viewRegister, 650, 450);
+        Stage window = (Stage) ((Node) register.getSource()).getScene().getWindow();
+        window.setScene(RegisterScene);
+        window.show();
+    }
+
+    public static String getLoggedUsername() { return loggedUsername;}
 }
