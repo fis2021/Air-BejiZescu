e007d386a0c8f1a09acab8552f0ea5717065ee83
SirBeji <robertul11356@gmail.com>
2021-05-09 18:23:20 +0300

    [ABZ-51] Cycle Functionality Done.

--- src/main/java/org/reg/controllers/CustomerPageController.java
+++ src/main/java/org/reg/controllers/CustomerPageController.java
@@ -1,8 +1,14 @@
 package org.reg.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CustomerPageController {
 
@@ -18,8 +24,13 @@ public class CustomerPageController {
     private Button accountButton;
 
     @FXML
-    public void handleLogout() {
-        Stage stage = (Stage) logoutButton.getScene().getWindow();
-        stage.close();
+    public void handleLogout(javafx.event.ActionEvent login) throws IOException {
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getClassLoader().getResource("login.fxml"));
+        Parent viewLogin = Loader.load();
+        Scene LoginScene = new Scene(viewLogin, 650, 450);
+        Stage window = (Stage) ((Node) login.getSource()).getScene().getWindow();
+        window.setScene(LoginScene);
+        window.show();
     }
 }
