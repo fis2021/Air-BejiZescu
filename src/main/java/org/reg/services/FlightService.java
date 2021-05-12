a79585bb8a46a494feea27c2357b7126f485ac9d
SirBeji <robertul11356@gmail.com>
2021-05-12 16:50:13 +0300

    [ABZ-55] Flight Model Done.

---
+++ src/main/java/org/reg/services/FlightService.java
@@ -0,0 +1,11 @@
+package org.reg.services;
+
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.reg.model.Flight;
+
+import static org.reg.services.FileSystemService.getPathToFlight;
+
+public class FlightService {
+
+}
